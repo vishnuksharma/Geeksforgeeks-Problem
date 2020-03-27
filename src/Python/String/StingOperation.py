f76c89d221246a7b40ac7e4d84be4f673cd2f728
Vishnu <vencn9@sg.uob>
2020-03-27 10:29:35 +0800

    added python practice

---
+++ src/Python/String/StingOperation.py
@@ -0,0 +1,12 @@
+pyStr = "this is python string"
+
+print(pyStr)
+
+# access char of sting
+print(pyStr[0]) # this will give 0 index
+
+# slice Sting
+print(pyStr[:4]) # return string from 0 index to 4th - 1 string
+
+print(pyStr[3:15]) # retrun string 3 to 15th - 1 index.
+
